cc2f2ddae6d819e9c8acebc222c56bbb6a6d607a
rcharan <google-dl-platform@googlegroups.com>
2020-01-17 20:43:38 +0000

    finish dreaming on inception

--- utilities/keras_layers.py
+++ utilities/keras_layers.py
@@ -9,7 +9,7 @@ def gram_matrix(activations):
     input_shape   = tf.shape(activations)
     num_locations = tf.cast(input_shape[1]*input_shape[2], tf.float32)
     return result/(num_locations)
-
+ 
 # Source layers (with no/fake inputs)
 class Source(Layer):
 
@@ -19,14 +19,16 @@ class Source(Layer):
         super().__init__(**kwargs)
 
     def build(self, input_shapes):
-        self.kernel = self.add_weight(name='kernel', shape=self.output_dim, initializer='uniform', trainable=True)
+        self.kernel = self.add_weight(name='kernel',
+                                      shape=self.output_dim,
+                                      initializer='uniform',
+                                      trainable=True,
+                                      constraint=self.kernel_constraint,
+                                     )
         super().build(input_shapes)
 
     def call(self, inputs):
-        if self.kernel_constraint is not None:
-            return self.kernel_constraint(self.kernel)
-        else:
-            return self.kernel
+        return self.kernel
 
     def compute_output_shape(self):
         return self.output_dim
@@ -42,11 +44,11 @@ def get_image_from_model(model, layer_name = 'image', format_ = 'vgg19'):
     out_img = model.get_layer(layer_name).get_weights()[0]
     if format_ == 'vgg19':
         out_img = vgg19_deprocess_image(out_img, clip_and_cast = False)
+        out_img = out_img - tf.reduce_min(out_img)
+        out_img = out_img / tf.reduce_max(out_img)
+        out_img = out_img * 256
     elif format_ == 'inceptionV3':
         out_img = inceptionV3_deprocess_image(out_img)
     out_img = tf.squeeze(out_img)
-    out_img = out_img - tf.reduce_min(out_img)
-    out_img = out_img / tf.reduce_max(out_img)
-    out_img = out_img * 256
     out_img = tf.cast(out_img, tf.uint8)
     return out_img
